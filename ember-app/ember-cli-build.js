16578005ead7753c6a50b21cadbab029eff4b39a
Turuntsev Konstantin <kturuntsev@ics.perm.ru>
2020-11-06 15:13:54 +0500

    добавил semanticUI

--- ember-app/ember-cli-build.js
+++ ember-app/ember-cli-build.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -20,5 +21,21 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  let extraAssets = new Funnel('semantic/dist/themes', {
+    srcDir: '/',
+    include: ['**/*'],
+    destDir: '/assets/themes'
+  });
+
+  app.import({
+    development: 'vendor/semantic.css',
+    production: 'vendor/semantic.min.css'
+  });
+
+  app.import({
+    development: 'vendor/semantic.js',
+    production: 'vendor/semantic.min.js'
+  });
+
+  return app.toTree(extraAssets);
 };
